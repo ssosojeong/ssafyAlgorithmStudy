9c22f0e9813c4bac14096639531955331f727c2d
HyeuongKyun <97081506+HyeuongKyun@users.noreply.github.com>
2022-08-07 17:27:54 +0900

    Add files via upload

---
+++ src/kimhyungkyun/week2/P2304.java
@@ -0,0 +1,105 @@
+package week2;
+
+import java.util.Scanner;
+
+public class P2304 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		//기둥의 갯수 N 
+		int N = sc.nextInt();
+		//좌표에 정보를 담고 있을 2차원 배열 생성
+		int [][] infoArray = new int[N][2];
+		
+		//제일 높은 기둥의 정보를 저장해둘 변수 생성
+		int heightest=0;
+		int heightestX=0;
+		//정보 입력 및 hieghtest의 정보 저장
+		for (int i=0; i<N ; i++) {
+			infoArray[i][0] = sc.nextInt();
+			infoArray[i][1] = sc.nextInt();
+			if (heightest< infoArray[i][1]) {
+				heightest=infoArray[i][1];
+				heightestX=infoArray[i][0];
+			}
+		}
+		
+		//들어오는 값 x값 기준 오름차순으로 정렬
+		for (int i=0;i<N-1;i++) {
+			for(int j=i+1;j<N;j++) {
+				if(infoArray[i][0]>infoArray[j][0]) {
+					//일시적으로 값을 저장해줄 변수 x,y
+					int x = infoArray[i][0];
+					int y = infoArray[i][1];
+					//i번째 있는 배열과 j번째 있는 배열값 변경
+					infoArray[i][0] = infoArray[j][0];
+					infoArray[i][1] = infoArray[j][1];
+					infoArray[j][0] = x;
+					infoArray[j][1] = y;
+				}
+			}
+		}
+		
+		//제일 높은 곳의 인덱스 찾기
+		int heightestIndex = -1;
+		for(int i=0;i<N;i++) {
+			if(infoArray[i][0] == heightestX) {
+				heightestIndex = i;
+				break;
+			}
+		}
+		
+//		//2중배열 중간점검 삭제 필
+//		for(int i=0;i<N;i++) {
+//			System.out.printf("%2d %2d\n",infoArray[i][0],infoArray[i][1]);
+//		}
+		//전체 넓이
+		int area = 0;
+		//heightestIndex 전까지 넓이 구하기
+		int beforeArea = 0;
+		int nextIndexBefore=-1;
+		for (int i=0;i<heightestIndex;i=nextIndexBefore) {
+			//다음 막대기가 전의 막대기 보다 길이가 긴지 확인 짧으면 패스
+			for(int j =i+1;j<=heightestIndex;j++) {
+				//i번째보다 긴 막대기 찾기
+				if(infoArray[i][1]<infoArray[j][1]) {
+					nextIndexBefore = j;
+					break;
+				}
+			}
+			int width = infoArray[nextIndexBefore][0]-infoArray[i][0];
+			beforeArea += width*infoArray[i][1];
+			if (nextIndexBefore == heightestIndex) break;
+		}
+		area += beforeArea;
+		
+		//heightest 까지 더하기
+		area += heightest;
+		
+		//heightest 이후의 넓이 더하기
+		int afterArea =0;
+		int nextIndexAfter=-1;
+		for (int i=N-1;i>heightestIndex;i=nextIndexAfter) {
+			//다음 막대기가 전의 막대기 보다 길이가 긴지 확인 짧으면 패스
+			for(int j =i-1;j>=heightestIndex;j--) {
+				//i번째보다 긴 막대기 찾기
+				if(infoArray[i][1]<infoArray[j][1]) {
+					nextIndexAfter = j;
+					break;
+				}
+			}
+			int width = infoArray[i][0]-infoArray[nextIndexAfter][0];
+			afterArea += width*infoArray[i][1];
+			
+			if (nextIndexAfter == heightestIndex) break;
+		}
+		
+		area += afterArea;
+		
+		
+		System.out.println(area);
+		
+		sc.close();
+		
+	}
+}
---
+++ src/kimhyungkyun/week2/P2605.java
@@ -0,0 +1,39 @@
+package week2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P2605 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//학생의 수 N 받기
+		int N = sc.nextInt();
+		//처음 줄 선 순서대로 학생들의 위치를 나타내줄 배열
+		List<Integer> studentList = new ArrayList<>();
+		//뽑은 번호 입력 받기
+		int[] number= new int[N];
+		for(int i=0; i<N;i++) {
+			number[i]=sc.nextInt();
+			studentList.add(i+1);
+		}
+		
+		//뽑은 수만큼 앞에 있는 사람이랑 자리 바꾸기(제일 앞자리는 무조건 0을 뽑으니까 인덱스가 1일 때부터 실행)
+		for(int i=1;i<N;i++) {
+			change(studentList, i-number[i],i);
+		}
+		
+		for(int i=0; i<N;i++) {
+			System.out.printf("%d ",studentList.get(i));
+		}
+		
+		sc.close();
+	}
+	
+	//배열의 값을 바꿔줄 함수(매개변수는 바꿀 배열, 나랑 자리를 바꿀 앞에 서있는 사람 인덱스 , 내 자리 인덱스)
+	public static void change(List<Integer> studentList,int indexOther , int indexMe) {
+		studentList.add(indexOther,studentList.get(indexMe));
+		studentList.remove(indexMe+1);
+	}
+}
