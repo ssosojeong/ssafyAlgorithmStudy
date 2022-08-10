46b099a1c71e14e8a1ce10dc6940d34873ab12c3
uyk1 <109265413+uyk1@users.noreply.github.com>
2022-08-10 18:54:48 +0900

    Update 창고다각형_2304.java
    답답

--- src/ohhyunkyu/week2/창고다각형_2304.java
+++ src/ohhyunkyu/week2/창고다각형_2304.java
@@ -1,8 +1,7 @@
-// 실패해서 재도전 중.
-
-package week2;
+// 왜 안될까..
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,24 +20,33 @@ public class 창고다각형_2304 {
 		// 가장 높은 기둥 찾기;
 		int max = 0;
 
-		List<Integer> maxIdx = new ArrayList<>();
-
-		int idx = 0;
-
 		for (int i = 0; i < 1001; i++) {
 			if (wh[i] >= max) {
 				max = wh[i]; // 기둥 최고점 높이
-				maxIdx.add(idx++, i); // 가장 높은 기둥들 위치
+//				maxIdx.add(idx++, i); // 가장 높은 기둥들 위치
+				//.... 를 따로 구해야 하네. 여기서 하면 max 바뀔 때마다 들어감.
 			}
 		}
+		System.out.println(max);
+		
+		int idx = 0; // 최고 높이 기둥들의 위치를 찾아보자.
 
+		List<Integer> maxIdx = new ArrayList<>();
+		
+		for (int i = 0; i < 1001; i++) {
+			if (wh[i] == max) {
+				maxIdx.add(idx++, i);
+			}
+		} // 이제야 제대로 위치 잡힘.
+		
+		
 		int lh = 0; // 왼쪽에서 기둥 높이 측정
 
 		List<Integer> lIdx = new ArrayList<>();
 
-		int lidx = 0; 
+		int lidx = 0;
 
-		for (int i = 0; i < maxIdx.get(0); i++) {
+		for (int i = 0; i <= maxIdx.get(0); i++) {
 			if (wh[i] > lh) {
 				lh = wh[i];
 				lIdx.add(lidx++, i); // 높이가 달라질 때 기둥 위치 저장
@@ -46,43 +54,54 @@ public class 창고다각형_2304 {
 		}
 
 		int lsum = 0; // 왼쪽면적 총합
-
-		for (int i = 0; i < lIdx.size(); i++) {
-			lsum += wh[lIdx.get(i)] * (lIdx.get(i + 1)- lIdx.get(i));
-		}
 		
-
+		// 높이가 달라지는 경우의 수 별로 나누어 계산
+		if(lidx >= 1) {
+			for (int i = 0; i < lIdx.size()-1; i++) {
+				lsum += wh[lIdx.get(i)] * (lIdx.get(i + 1) - lIdx.get(i));
+			}
+		}else if (lidx == 0) {
+			lsum = wh[lIdx.get(0)] * (maxIdx.get(0) - lIdx.get(0));
+		}
+		System.out.println(lsum);
+		
 		int rh = 0; // 오른쪽에서 기둥 높이 측정
 
 		List<Integer> rIdx = new ArrayList<>();
 
 		int ridx = 0;
 
-		for (int i = 1000; i > maxIdx.get(maxIdx.size()-1); i--) {
+		for (int i = 1000; i >= maxIdx.get(maxIdx.size() - 1); i--) { 
 			if (wh[i] > rh) {
 				rh = wh[i];
 				rIdx.add(ridx++, i); // 높이가 달라질 때 기둥 위치 저장
 			}
 		}
-
+		
 		int rsum = 0; // 오른쪽 면적 총합
 
-		for (int i = 1; i < rIdx.size(); i++) {
-			rsum += wh[rIdx.get(i-1)] * ((rIdx.get(i-1)+1) - rIdx.get(i));
+		// 높이가 달라지는 경우의 수 별로 나누어 계산
+		if(ridx >= 1) {
+			for (int i = 0; i < rIdx.size()-1; i++) {
+				rsum += wh[rIdx.get(i)] * ((rIdx.get(i)) - rIdx.get(i+1));
+			}
+		}else if(ridx == 0) {
+			rsum = wh[rIdx.get(0)] * ((rIdx.get(0)) - maxIdx.get(maxIdx.size()-1));
 		}
+		System.out.println(rsum);
 		
-		//가장 높은 기둥들 추가해서 면적구하기
-		
+		// 가장 높은 기둥들 추가해서 면적구하기
+
 		int res = lsum + rsum;
-		
-		if(maxIdx.size() == 1) { // 가장 높은 기둥이 하나
+
+		if (maxIdx.size() == 1) { // 가장 높은 기둥이 하나
 			res += max;
-		}else { // 둘 이상
-			res += max * ((maxIdx.get(maxIdx.size()-1)+1)-maxIdx.get(0));
+		} else { // 둘 이상
+			res += max * ((maxIdx.get(maxIdx.size() - 1)) - maxIdx.get(0));
 		}
-		
+
 		System.out.println(res);
-		
+
 	} // main
 } // class
 
