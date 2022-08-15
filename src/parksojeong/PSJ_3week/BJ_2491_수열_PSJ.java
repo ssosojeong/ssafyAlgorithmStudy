292e2052c0018b26f904d5c1aaa700e556878b6c
gosjgo <99484238+gosjgo@users.noreply.github.com>
2022-08-15 21:20:54 +0900

    PSJ_수열풀었습니다

--- src/parksojeong/PSJ_3week/BJ_2491_수열_PSJ.java
+++ src/parksojeong/PSJ_3week/BJ_2491_수열_PSJ.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
-// 늘어나는 것만 하고 줄어드는건 놓칠뻔했어요 문제를 꼼꼼히 읽는게 중요걸 알았습죠!
+// 늘어나는 것만 하고 줄어드는건 놓칠뻔했어요 문제를 꼼꼼히 읽는게 중요하다는걸 알았습죠!
 public class BJ_2491_수열_PSJ {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
