87cc858a971b9345ed46dbf398ad5de398766f40
gosjgo <99484238+gosjgo@users.noreply.github.com>
2022-09-30 00:02:19 +0900

    Update BJ_09월29일_스위치켜고끄기_1244.java

--- src/parksojeong/PSJ_IM재시험 대비/BJ_09월29일_스위치켜고끄기_1244.java
+++ src/parksojeong/PSJ_IM재시험 대비/BJ_09월29일_스위치켜고끄기_1244.java
@@ -22,20 +22,20 @@ public class BJ_스위치켜고끄기_1244 {
 		for (int tc = 1; tc <= students; tc++) {// 테케
 			int sex = sc.nextInt();
 			int num = sc.nextInt();
-			if (sex == 1) {
+			if (sex == 1) { //성별이 남자일때 여자일때 함수로 연결
 				boy(num);
 			} else {
 				girl(num);
 			}
 		} // 테케
-		for (int i = 1; i < switchs.length; i++) {
+		for (int i = 1; i < switchs.length; i++) { //출력
 			System.out.print(switchs[i]+" ");
-			if(i%20 == 0) {
+			if(i%20 == 0) { //20개 넘어가면 줄바꿈 이거안해서 계속 에러났다.
 			System.out.println();
 			}
-		}
+		}//출력
 
-	}
+	}//main
 
 	private static void girl(int num) { // girl 함수
 		switchs[num] = 1 - switchs[num]; //스위치 바꿔주기
@@ -47,8 +47,8 @@ public class BJ_스위치켜고끄기_1244 {
 		}
 		for (int i = 0; i <= range; i++) {//좌우대칭 확인
 			if (switchs[num - i] == switchs[num + i]) {
-				switchs[num - i] = 1 - switchs[num - i];
-				switchs[num + i] = 1 - switchs[num + i];
+				switchs[num - i] = 1 - switchs[num - i]; // 왼쪽 스위치 바꾸기
+				switchs[num + i] = 1 - switchs[num + i]; // 오른쪽 스위치 바꾸기
 			} else {//좌우대칭 아니면 빠져나온다.
 				break;
 			}
@@ -57,7 +57,7 @@ public class BJ_스위치켜고끄기_1244 {
 	}
 
 	private static void boy(int num) { // boy
-		for (int i = 1; i <= count / num; i++) { // 배수가 될때 스위치를 바꿔준다.
+		for (int i = 1; i <= count / num; i++) { // 배수가 될때 스위치를 바꿔준다. 배수의 개수를 찾기 위해 count/num
 			switchs[i * num] = 1 - switchs[i * num];
 		}
 
